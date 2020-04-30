ae17f3c3fb7e4c97465048608b19af41e526036d
sumitshengokar <64583358+sumitshengokar@users.noreply.github.com>
2020-04-30 23:19:59 +0530

    Adding Streamlistener api and twitter authentication developer code
    Each user should generate his own twitter developer API key's to get access to the twitter data

---
+++ tweepy_tutorial_copy.py
@@ -0,0 +1,117 @@
+import sys
+import datetime
+from tweepy import API,Cursor
+from tweepy import OAuthHandler
+from tweepy import API
+from tweepy import Stream
+from tweepy.streaming import StreamListener
+import pymysql
+
+
+# Replace the "None"s by your own credentials
+ACCESS_TOKEN ='XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+ACCESS_TOKEN_SECRET ='YYYYYYYYYYYYYYY'
+CONSUMER_KEY='ZZZZZZZZZZZZZZZZZZZZZZZ'
+CONSUMER_SECRET='SSSSSSSSSSSSSSSSSSSSSSSSSSs'
+
+#### TWITTER AUTHENTICATOR
+
+
+
+auth = OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+api = API(auth, wait_on_rate_limit=True,
+          wait_on_rate_limit_notify=True)
+
+
+
+def add_in_db(retweet_count, text, create, likes, location, source, reply, retweet):
+    sql = "INSERT INTO Tweets (retweet_count,tweet,created_time,likes,location,source,reply,retweeted) VALUES(%s,%s,%s,%s,%s,%s,%s,%s)"
+    val = [retweet_count, text, create, likes, location, source, reply, retweet]
+    mycursor.execute(sql, val)
+    conn.commit()
+
+
+def run_code():
+    # Replace the "None"s by your own credentials
+    ACCESS_TOKEN = '1226756571499089922-9k7CYMcwiassaccMl7KHONByoEVVYH'
+    ACCESS_TOKEN_SECRET = 'W5B6FYKJ1sHPV9iZ23IJyH7jnw07lTZPkQ1whXHBwpTDx'
+    CONSUMER_KEY = 'LHIbifnCRZHleOt4mI8e4Qdtg'
+    CONSUMER_SECRET = 'kwItWpU4cFEKV7EMVqmg2OIqCzT3XjH5o3QmwRQP6cUCseUSfA'
+
+    auth = OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+    api = API(auth, wait_on_rate_limit=True,
+              wait_on_rate_limit_notify=True)
+
+    conn = pymysql.connect(host="localhost", user="root", passwd="", db="tweets_db")
+    mycursor = conn.cursor()
+
+    class Listener(StreamListener):
+        def __init__(self, output_file=sys.stdout):
+            super(Listener, self).__init__()
+
+        def add_in_db(retweet_count, text,create,likes,location,source,reply,retweet):
+            sql = "INSERT INTO Tweets (retweet_count,tweet,created_time,likes,location,source,reply,retweeted) VALUES(%s,%s,%s,%s,%s,%s,%s,%s)"
+            val = [retweet_count, text, create, likes, location, source, reply, retweet]
+            mycursor.execute(sql, val)
+            conn.commit()
+
+        def on_status(self, status):
+            text = status.text
+            retweet_count = status.retweet_count
+            create = status.created_at
+            likes = status.favorite_count
+            source = status.source
+            location = status.user.location
+            retweet = "False"
+            try:
+                if status.retweeted_status:
+                    retweet = "True"
+                    likes = status.retweeted_status.favorite_count
+            except:
+                retweet = "False"
+            reply = "False"
+            try:
+                if status.in_reply_to_screen_name:
+                    reply = "True"
+            except:
+                reply = "False"
+
+            try:
+                if not location:
+                    location = "None"
+            except:
+                location = status.user.location
+
+            print(text)
+            add_in_db(retweet_count, text,create,likes,location,source,reply,retweet)
+
+
+        def on_error(self, status_code):
+            print("Encountered streaming error",status_code)
+            return False
+
+
+    listener = Listener()
+
+    stream = Stream(auth=api.auth, listener=listener,tweet_mode='extended')
+    tags = ['#kashmir','#Brexit',#india','#corona', '#modi', '#china', '#WHO', '#maharashtra', '#pune','#mumbai',
+            '#PM','#actor', '#CM', '#pakistan','#trump']
+    stream.filter(track=tags)
+    try:
+        print('Start streaming.')
+        stream.sample(languages=['en'])
+    except KeyboardInterrupt:
+        print("Stopped.")
+        conn.close()
+    finally:
+        print('Done.')
+        conn.close()
+        stream.disconnect()
+
+
+if __name__=="__main__":
+    conn = pymysql.connect(host="localhost", user="root", passwd="", db="tweets_db")
+    mycursor = conn.cursor()
+    run_code()
